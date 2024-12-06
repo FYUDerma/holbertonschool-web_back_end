15d335771248a9380a2aad9392cf4565a0ae6f9b
FYUDerma <a.zakaria.pro@gmail.com>
2024-12-06 14:32:48 +0100

    feat: add sum_mixed_list function and main file for testing mixed list sums

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Complex types - mixed list """
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lstl: list[int | float]) -> float:
+    """ Returns the sum of the list of floats. """
+    return sum(mxd_lstl)
