5e26fb80ed554e41e3855aab4334d900759232cb
FYUDerma <a.zakaria.pro@gmail.com>
2024-12-06 14:22:18 +0100

    feat: add floor function with type annotations and main file for testing

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Basic annotations - floor """
+
+
+def floor(n: float) -> int:
+    """
+    Returns the largest integer less than or equal to n.
+    """
+    return int(n)
