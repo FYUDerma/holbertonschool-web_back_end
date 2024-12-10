cad441e44917c46e3eaa10ea9e340431390d491c
FYUDerma <a.zakaria.pro@gmail.com>
2024-12-10 05:03:21 +0100

    feat: add wait_n coroutine to return a list of delays in ascending order

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Concurrent coroutines"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ Returns a list of delays in ascending order """
+    delays = [wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
