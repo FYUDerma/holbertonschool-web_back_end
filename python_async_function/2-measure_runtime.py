7df2cd6c5119f61c0f0b19325e31b7b5641704a1
FYUDerma <a.zakaria.pro@gmail.com>
2024-12-10 05:13:28 +0100

    feat: add measure_time function to calculate average runtime of wait_n coroutine

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Measure runtime"""
+import asyncio
+import time
+from typing import List
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """Mesure the runtime"""
+    start = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.perf_counter() - start
+    return total_time / n
