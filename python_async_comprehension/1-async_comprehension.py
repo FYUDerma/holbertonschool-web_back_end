04bc13e9152cb1ad452a86ef307c2ab542b74bcf
FYUDerma <a.zakaria.pro@gmail.com>
2024-12-11 14:27:48 +0100

    fix: add type hint for async_comprehension function

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,9 +2,10 @@
 """Async Comprehensions"""
 import asyncio
 import random
+import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> typing.List[float]:
     """Collect 10 random numbers using an async comprehensing"""
     return [i async for i in async_generator()]
