2c257f10ebd290bf7906c35b28a70edf4975e602
FYUDerma <a.zakaria.pro@gmail.com>
2024-12-11 11:50:40 +0100

    docs: simplify docstring for async_generator to focus on core functionality

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,9 +6,7 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Async Generator with a coroutine called async_generator that takes no arguments.
-    The coroutine will loop 10 times, each time asynchronously wait 1 second, 
-    then yield a random number between 0 and 10. Use the random module."""
+    """Async Generator"""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
