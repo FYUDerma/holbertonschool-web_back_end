4ae36017d306f199b15c5a75bf25f6ea72865938
FYUDerma <a.zakaria.pro@gmail.com>
2024-12-11 14:05:21 +0100

    fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """Async Generator"""
     for _ in range(10):
         yield random.uniform(0, 10)
