a348688fcad73e4c51c8d4916a74e8dfad2b7186
FYUDerma <a.zakaria.pro@gmail.com>
2024-12-13 17:43:28 +0100

    style: enhance docstrings in log_stats module for clarity and detail

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,40 +1,54 @@
 #!/usr/bin/env python3
-"""Log Stats"""
+"""Log Stats
+
+This module provides statistics about Nginx logs stored in MongoDB.
+"""
+
 from pymongo import MongoClient
 
 
 def log_stats():
-    """Log Stats"""
+    """Prints statistics about Nginx logs.
+
+    This function connects to a MongoDB database, retrieves logs,
+    and prints the total number of logs, the count of different HTTP
+    methods, and the number of status check requests.
+    """
     client = MongoClient()
     logs = client.logs.nginx
 
-    """Number of documents in logs collection"""
+    # Number of documents in logs collection
     print(f'{logs.count_documents({})} logs')
 
     print('Methods:')
 
-    """Number of documents with method=GET"""
+    # Number of documents with method=GET
     method_get = logs.count_documents({"method": "GET"})
     print(f'\tmethod GET: {method_get}')
-    """Number of documents with method=POST"""
+    
+    # Number of documents with method=POST
     method_post = logs.count_documents({"method": "POST"})
     print(f'\tmethod POST: {method_post}')
-    """Number of documents with method=PUT"""
+    
+    # Number of documents with method=PUT
     method_put = logs.count_documents({"method": "PUT"})
     print(f'\tmethod PUT: {method_put}')
-    """Number of documents with method=PATCH"""
+    
+    # Number of documents with method=PATCH
     method_patch = logs.count_documents({"method": "PATCH"})
     print(f'\tmethod PATCH: {method_patch}')
-    """Number of documents with method=DELETE"""
+    
+    # Number of documents with method=DELETE
     method_delete = logs.count_documents({"method": "DELETE"})
     print(f'\tmethod DELETE: {method_delete}')
 
-    """Number of documents with method=GET and path=/status"""
+    # Number of documents with method=GET and path=/status
     method_get_path = logs.count_documents({
         "method": "GET",
         "path": "/status"
-        })
+    })
     print(f'{method_get_path} status check')
+
 
 
 if __name__ == "__main__":
