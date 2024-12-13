968291fed47d96a00eba5738f93dfd3066e380f6
FYUDerma <a.zakaria.pro@gmail.com>
2024-12-13 07:56:26 +0100

    fix: update return value of update_topics function to reflect update operation

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -4,5 +4,4 @@
 
 def update_topics(mongo_collection, name, topics):
     """Changes all topics of a school document based on the name"""
-    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
-    return mongo_collection.find_one({"name": name})
+    return mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
