d9c8594727e660e1bd7204cd27ea5c716559e26d
FYUDerma <a.zakaria.pro@gmail.com>
2024-12-13 20:06:41 +0100

    fix: streamline MongoDB connection and improve string formatting in log_stats function

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,23 +6,22 @@ from pymongo import MongoClient
 def log_stats():
     """Log Stats"""
     client = MongoClient()
-    db = client.logs
-    logs = db.nginx
+    logs = client.logs.nginx
 
     total_logs = logs.count_documents({})
-    print(f'{total_logs} logs')
+    print(f"{total_logs} logs")
 
-    print('Methods:')
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = logs.count_documents({'method': method})
-        print(f'\tmethod {method}: {count}')
+        count = logs.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
     status_check = logs.count_documents({
-        'method': 'GET',
-        'path': '/status'
+        "method": "GET",
+        "path": "/status"
         })
-    print(f'{status_check} status check')
+    print(f"{status_check} status check")
 
 
 if __name__ == "__main__":
