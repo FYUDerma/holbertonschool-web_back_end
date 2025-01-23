c40b7d62333876d2b8e89e49f16a8facc965b39d
FYUDerma <a.zakaria.pro@gmail.com>
2025-01-23 15:58:34 +0100

    feat: export HTTP server module for external use

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,7 +1,8 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello Holberton School!');
     res.end();
 }).listen(1245);
+
+module.exports = http;
