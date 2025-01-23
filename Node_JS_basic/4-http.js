5b04949898662a3debef2ffa0ae2c96cd806de8b
FYUDerma <a.zakaria.pro@gmail.com>
2025-01-23 16:04:15 +0100

    refactor: use const for HTTP module and improve formatting in server response

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,9 +1,9 @@
-var http = require('http');
+const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello Holberton School!');
-    res.end();
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('Hello Holberton School!');
+  res.end();
 });
 
 module.exports = server;
