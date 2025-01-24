0d9397ed53fa2d3811a96c94f2ee7f556c84a0d8
FYUDerma <a.zakaria.pro@gmail.com>
2025-01-24 10:26:43 +0100

    refactor: rename app variable to server and update server response handling

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const port = 1245;
-const app = express();
+const server = express();
 
-app.get('/', (req, res) => {
+const app = server.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 }).listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
+module.exports = app;
